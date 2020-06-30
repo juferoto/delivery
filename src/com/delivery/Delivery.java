27f30cca6faffdaa94b7249ffa6d1a221fec13c4
juanrodrigueztorres <juanrodriguez@heinsohn.com.co>
2020-06-30 00:22:21 -0500

    DeliveryApplication
    - Added the main class

---
+++ src/com/delivery/Delivery.java
@@ -0,0 +1,19 @@
+package com.delivery;
+
+/**
+ * Main class that executes the program
+ * 
+ * @author Juan Rodriguez
+ *
+ */
+public class Delivery
+{
+	// Path where is located all routes for drones
+	private static final String PATH = "C:\\Users\\FamiliaRR\\Documents\\Delivery";
+
+	public static void main(String[] args)
+	{
+		System.out.println("Main class");
+	}
+
+}
